e2d7e7f59804a102eb9fdbb7b2b79beca8c98fb2
6u1lh3rm3 <124220438+6u1lh3rm3@users.noreply.github.com>
2024-09-07 18:08:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,93 @@
+let dados = [
+    {
+        titulo: "Rafaella Ballerini",
+        descricao: "Cursei Medicina por três anos até descobrir minha paixão: desenvolvimento de software. Hoje trabalho como criadora de conteúdo de programação e tecnologia, guiando pessoas que desejam iniciar na área de desenvolvimento de software. Possuo um canal no Youtube com mais de 300 mil inscritos e tenho mais de 150 mil pessoas acompanhando meu conteúdo no Instagram. Sou fundadora de uma comunidade de mais de 40 mil membros: Comunidade Ballerini, onde compartilhamos conhecimento sobre as diferentes áreas da programação, tiramos dúvidas e fazemos networking. Já trabalhei como instrutora front-end na Alura, uma das maiores escolas de tecnologia do Brasil.",
+        link: "https://www.linkedin.com/in/rafaellaballerini/",
+        imagem: "image\\rafa.jpg"
+    },
+    {
+        titulo: "Guilherme Lima",
+        descricao: "Desenvolvedor de software experiente com Bacharelado em Tecnologia da Informação e mais de 4 anos de experiência em codificação usando tecnologias Python, Django, Google GO, JavaScript e Ruby. Também criou vários cursos de TI em altas tecnologias para mais de 10 mil alunos de TI usando a plataforma Alura. Garantindo a qualidade, desenvolvendo estratégias para dar suporte a aplicações de sistema, gerenciamento de banco de dados e proporcionando uma experiência positiva ao usuário. Destaques da carreira: Desenvolveu 60 cursos de sucesso focados em capacitação de profissionais de TI em Python e Google Go. Codificou 20 projetos usando altas tecnologias como Ruby, Python e Google Go. Experiência avançada em programação em Python, JavaScript e Google GO. Trabalha com uma equipe multifuncional ágil.",
+        link: "https://www.linkedin.com/in/guilherme-lima-developer/",
+        imagem: "image\\gui.jpg"
+    },
+    {
+        titulo: "Luciano Martins",
+        descricao: "No Google desde fevereiro de 2020, estou imerso no mundo de Cloud AI, trabalho como facilitador para a aplicação de IA no mundo real. Minha função como Developer Advocate combina profundo conhecimento técnico com paixão por capacitar a comunidade de desenvolvedores. Utilizando as tecnologias de Google AI, facilito a adoção de tecnologias de IA que impulsionam a inovação e a eficiência. Também Já atuei como especialista em machine learning e engenheiro de software, dedicado a implantação de soluções de IA que aprimoram os ambientes empresariais. Minha abordagem é prática, com foco no fornecimento de arquiteturas robustas e escaláveis ​​baseadas em IA que atendam às necessidades críticas dos negócios. A combinação da minha experiência técnica e a abordagem de Developer Advocacy é a ferramenta que me permite seguir na busca de ser um agente ativo nos avanços da adoção da IA no mundo real.",
+        link: "https://www.linkedin.com/in/lucianommartins/",
+        imagem: "image\\lu.jpg"
+
+    },
+    {
+        titulo: "Nikola Tesla",
+        descricao: "Nikola Tesla, um dos maiores gênios da eletricidade, revolucionou o mundo com suas invenções. Nascido na Sérvia, Tesla é mais conhecido por suas contribuições ao desenvolvimento do sistema de fornecimento de eletricidade em corrente alternada (CA), que utilizamos até hoje. Suas ideias visionárias e experimentos ousados, como a famosa Bobina de Tesla, o projetaram como um dos cientistas mais influentes de todos os tempos.",
+        link: "https://pt.wikipedia.org/wiki/Nikola_Tesla",
+        imagem: "image\\tesla.jpeg"
+    },
+    {
+        titulo: "Albert Einstein",
+        descricao: "Físico teórico alemão, considerado um dos maiores gênios de todos os tempos. Desenvolveu a teoria da relatividade geral, que revolucionou a compreensão do espaço, tempo e gravidade. Ganhou o Prêmio Nobel de Física de 1921 por suas explicações sobre o efeito fotoelétrico.",
+        link: "https://pt.wikipedia.org/wiki/Albert_Einstein",
+        imagem: "image\\einstein.jpg"
+    },
+    {
+        titulo: "Isaac Newton",
+        descricao: "Físico e matemático inglês, considerado um dos cientistas mais influentes de todos os tempos. Formulou as leis da mecânica clássica, a lei da gravitação universal e fez contribuições fundamentais para o desenvolvimento do cálculo infinitesimal.",
+        link: "https://pt.wikipedia.org/wiki/Isaac_Newton",
+        imagem: "image\\isaac_newton.jpg"
+    },      
+    {
+        titulo: "Alan Turing",
+        descricao: "Matemático e cientista da computação britânico, considerado o pai da ciência da computação moderna. Desenvolveu a máquina de Turing, um modelo teórico de computação que é fundamental para a ciência da computação moderna.",
+        link: "https://pt.wikipedia.org/wiki/Alan_Turing",
+        imagem: "image\\turing.webp"
+    },
+    {
+        titulo: "Grace Hopper",
+        descricao: "Cientista da computação norte-americana, pioneira na programação e desenvolvedora da linguagem de programação COBOL. É conhecida por popularizar o termo 'bug' em computação.",
+        link: "https://pt.wikipedia.org/wiki/Grace_Hopper",
+        imagem: "image\\grace.jpg"
+    },
+    {
+        titulo: "Ada Lovelace",
+        descricao: "Matemática inglesa, considerada a primeira programadora de computadores. Trabalhou com Charles Babbage na máquina analítica, antecessora dos computadores modernos.",
+        link: "https://pt.wikipedia.org/wiki/Ada_Lovelace",
+        imagem: "image\\grace.jpg"
+    },
+    {
+        titulo: "Stephen Hawking",
+        descricao: "Físico teórico britânico, conhecido por suas contribuições à cosmologia e à física teórica, especialmente no campo dos buracos negros.",
+        link: "https://pt.wikipedia.org/wiki/Stephen_Hawking",
+        imagem: "image\\stephen.jpg"
+    },
+    {
+        titulo: "Galileu Galilei",
+        descricao: "Astrônomo, físico e filósofo italiano, considerado o pai da ciência moderna. Revolucionou a astronomia e a física com suas observações e teorias.",
+        link: "https://pt.wikipedia.org/wiki/Galileu_Galilei",
+        imagem: "image\\galileu.jpg"
+    },
+    {
+        titulo: "Antoine Lavoisier",
+        descricao: "Químico francês, considerado o pai da química moderna. Descobriu a lei da conservação da massa e contribuiu para o desenvolvimento da nomenclatura química.",
+        link: "https://pt.wikipedia.org/wiki/Antoine_Lavoisier",
+        imagem: "image\\antoniue.jpg"
+    },
+    {
+        titulo: "Charles Darwin",
+        descricao: "Naturalista inglês, famoso por sua teoria da evolução das espécies por meio da seleção natural.",
+        link: "https://pt.wikipedia.org/wiki/Charles_Darwin",
+        imagem: "image\\darwin.jpg"
+    },
+    {
+        titulo: "Louis Pasteur",
+        descricao: "Cientista francês, fundador da microbiologia moderna e desenvolvedor da vacina contra a raiva.",
+        link: "https://pt.wikipedia.org/wiki/Louis_Pasteur",
+        imagem: "image\\pasteur.jpg"
+    },
+    {
+        titulo: "Rosalind Franklin",
+        descricao: "Cientista britânica, conhecida por suas contribuições para a descoberta da estrutura do DNA.",
+        link: "https://pt.wikipedia.org/wiki/Rosalind_Franklin",
+        imagem: "image\\rosalind.jpg"
+    }
+];
